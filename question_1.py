f70dc5291f1898b97de2d7493c463d4194976849
aksharavalsalakumar <50225187+aksharavalsalakumar@users.noreply.github.com>
2024-06-09 23:40:34 +0530

    Create question_1.py

---
+++ question_1.py
@@ -0,0 +1,24 @@
+#improved code
+def get_contents(fname):
+    try:
+        with open(fname) as f:
+            return f.read()
+    except FileNotFoundError:
+        return None
+
+
+#UNIT test
+def get_contents(fname):
+    try:
+        f = open(fname).read()
+        return f
+    except FileNotFoundError:
+        return None
+
+import pytest
+@pytest.mark.parametrize("fname, output",[("./aksharaa.txt",None),("./akshara.txt",['h', 'a', 'i', ' ', 'w', 'o', 'r', 'l', 'd', '\n', 'w', 'e', 'l', 'c', 'o', 'm', 'e', '\n'])])
+def test_multiplication_11(fname, output):
+    r = get_contents(fname)
+    if r!=None:
+        r=list(r)
+    assert r==output or r== None
